70f8b8c4e6d57bc0127a18eb18d0e6924f43dcdc
theoldtree <yjeongmu@gmail.com>
2023-10-16 23:27:33 +0900

    monitoring icon add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { TbHeartRateMonitor } from "react-icons/tb";
+
 export default function Home() {
   return (
     <div className="flex flex-row ">
       <div className="flex-1 border-solid border-2 border-black flex justify-center items-center h-screen">
+        <TbHeartRateMonitor size={200} />
         Hello World!!
       </div>
       <div className="flex-1 border-solid border-2 border-black flex justify-center items-center h-screen">
