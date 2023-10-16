925a04c076c9f9ac525bd5772cdf225f1b07098d
theoldtree <yjeongmu@gmail.com>
2023-10-16 23:45:46 +0900

    monitoring button add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,17 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-row ">
-      <div className="flex-1 flex-col border-solid border-2 border-black flex justify-center items-center h-screen">
+      <section className="flex-1 flex-col border-solid border-2 border-black flex justify-center items-center h-screen">
         <TbHeartRateMonitor size={200} />
-        <Link href="/client" className="button">
-          센서 모니터링
-        </Link>
-      </div>
-      <div className="flex-1 border-solid border-2 border-black flex justify-center items-center h-screen">
+        <div className="border-sky-700 border-solid border-2 rounded-md p-2">
+          <Link href="/client" className="button">
+            센서 모니터링
+          </Link>
+        </div>
+      </section>
+      <section className="flex-1 border-solid border-2 border-black flex justify-center items-center h-screen">
         Hello World!!
-      </div>
+      </section>
     </div>
   );
 }
