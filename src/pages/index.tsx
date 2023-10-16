05a51abe58d0ba14ab72aad43712590fca9977c6
theoldtree <yjeongmu@gmail.com>
2023-10-16 23:34:41 +0900

    page structure add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { TbHeartRateMonitor } from "react-icons/tb";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-row ">
-      <div className="flex-1 border-solid border-2 border-black flex justify-center items-center h-screen">
+      <div className="flex-1 flex-col border-solid border-2 border-black flex justify-center items-center h-screen">
         <TbHeartRateMonitor size={200} />
-        Hello World!!
+        <Link href="/client" className="button">
+          센서 모니터링
+        </Link>
       </div>
       <div className="flex-1 border-solid border-2 border-black flex justify-center items-center h-screen">
         Hello World!!
