204ab8f01e32e60e3ac17571bfac23feedd2c872
theoldtree <yjeongmu@gmail.com>
2023-10-17 23:05:27 +0900

    manager page layout add

--- src/pages/manager/index.tsx
+++ src/pages/manager/index.tsx
@@ -1,3 +1,11 @@
+import { Divider } from "@nextui-org/react";
 export default function Manager() {
-  return <>manager!!</>;
+  // TODO: manager 권한체크해서 없다면 메인 페이지로 이동
+  return (
+    <div className="flex h-screen w-screen">
+      <div style={{ width: "5%" }} className="border-2 border-black"></div>
+      <div style={{ width: "15%" }} className="border-2 border-black"></div>
+      <div style={{ width: "80%" }} className="border-2 border-black"></div>
+    </div>
+  );
 }
