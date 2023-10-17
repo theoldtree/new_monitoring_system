6449195ee9327dfe9a9f6641b3a3fe48c1ba437a
theoldtree <yjeongmu@gmail.com>
2023-10-18 00:15:23 +0900

    alarm table complete

--- src/components/alarmtab.tsx
+++ src/components/alarmtab.tsx
@@ -1,3 +1,32 @@
+import { Divider } from "@nextui-org/react";
+import { Table } from "./table";
+import { ManagerInputForm } from "./inputformManager";
+import Icon from "./icon";
+
 export default function AlarmTab() {
-  return <>alarmtab!!</>;
+  const exampleData = { key: "value" };
+  return (
+    <div>
+      <div>
+        <p className="m-1">알람 서비스 추가</p>
+        <div style={{ height: "60px" }} className="flex flex-row">
+          <Icon name={"SiKakaotalk"} onclick={() => {}} />
+          <Icon name={"MdEmail"} onclick={() => {}} />
+        </div>
+      </div>
+      <Divider className="m-3" />
+      <div>
+        <ManagerInputForm
+          label="Threshold 설정"
+          label2="온도"
+          label3="습도"
+          btnText="설정"
+        />
+      </div>
+      <Divider className="m-3" />
+      <div>
+        <Table label="Alarm Table" data={exampleData} />
+      </div>
+    </div>
+  );
 }
