aceb7074d57f7f612165fed3bf6184ccf00ecb6b
theoldtree <yjeongmu@gmail.com>
2023-10-17 20:16:18 +0900

    inputform component complete

--- src/components/inputform.tsx
+++ src/components/inputform.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 interface InputFormProps {
   label: string;
   label2: string;
@@ -13,17 +15,23 @@ export const InputForm: React.FC<InputFormProps> = ({
 }) => {
   return (
     <form>
-      <div>
-        <section>{label}</section>
-        <section>
+      <div className="border-2 border-black border-solid">
+        <section className="flex flex-col items-center py-2">
+          <label>{label}</label>
+        </section>
+        <section className="flex flex-col pl-2 justify-between w-64 border-2 border-black">
           <label>{label2}</label>
-          <input type="text" />
+          <input type="text" className="border-2 m-1 border-gray-400" />
         </section>
-        <section>
+        <section className="flex flex-col pl-2 justify-between w-64 border-2 border-black">
           <label>{label3}</label>
-          <input type="text" />
+          <input type="text" className="border-2 m-1 border-gray-400" />
         </section>
-        <button>{btnText}</button>
+        <div className="flex justify-end pr-2">
+          <button className=" border-sky-700 border-solid border-2 rounded-md p-2">
+            {btnText}
+          </button>
+        </div>
       </div>
     </form>
   );
