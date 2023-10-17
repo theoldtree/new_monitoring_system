9e363fe4788ed8b378b89566229c8bac6aa2f380
theoldtree <yjeongmu@gmail.com>
2023-10-17 22:45:49 +0900

    dashboard styling complete

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -5,7 +5,7 @@ interface TableProps {
 
 export const Table: React.FC<TableProps> = ({ data, label }) => {
   return (
-    <div className="flex-1 border-2 border-black items-center justify-center pt-2">
+    <div className="flex-1 border-2 border-black items-center justify-center pt-2 h-full">
       <label className="m-4 text-center">{label}</label>
       <div className="flex flex-col border-2 border-black p-3 w-5/6 h-5/6 mx-auto mt-3">
         {/* 예시: 객체의 key와 value를 출력 */}
